7cfb3417f6c3e917857e2341ad9b83c150074f7a
lucas-almeida-1 <luquinhas.al@hotmail.com>
2024-02-29 20:41:42 -0300

    Fixed other bugs

--- lib/login.dart
+++ lib/login.dart
@@ -2,14 +2,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
-// import pages  
+// import pages
 import 'register.dart';
 import 'home.dart';
 
 // ignore: use_key_in_widget_constructors
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
   // ignore: library_private_types_in_public_api
   _LoginPageState createState() => _LoginPageState();
@@ -41,14 +42,16 @@ class _LoginPageState extends State<LoginPage> {
       );
     } catch (e) {
       // ignore: use_build_context_synchronously
-      email: emailController.text,
-      password: passwordController.text,
-      );
+      emailController.text;
+      passwordController.text;
       Navigator.push(
+        // ignore: use_build_context_synchronously
         context,
-        MaterialPageRoute(builder: (context) => HomePage()),
+        MaterialPageRoute(builder: (context) => const HomePage()),
       );
+      // ignore: dead_code_catch_following_catch
     } catch (e) {
+      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Erro ao fazer login.'),
@@ -238,8 +241,7 @@ class _LoginPageState extends State<LoginPage> {
                                       _obscureText
                                           ? Icons.visibility_off
                                           : Icons.visibility,
-                                      color: const Color.fromARGB(
-                                          255, 57, 211, 54),
+                                      color: const Color.fromARGB(255, 57, 211, 54),
                                     ),
                                     onPressed: _togglePasswordVisibility,
                                   ),
@@ -247,8 +249,8 @@ class _LoginPageState extends State<LoginPage> {
                               ),
                               InkWell(
                                 onTap: () {
-                                  print(
-                                      'Usuário apertou "Esqueci minha senha"');
+                              //    print(
+                              //        'Usuário apertou "Esqueci minha senha"');
                                 },
                                 child: const Text(
                                   'Esqueci minha senha',
