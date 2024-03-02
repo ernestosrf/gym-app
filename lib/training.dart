001dfd2b5ae4c99fa282539feb7e317726be2121
lucas-almeida-1 <luquinhas.al@hotmail.com>
2024-03-02 09:26:22 -0300

    Clean problems in terminal

--- lib/training.dart
+++ lib/training.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, duplicate_import, unnecessary_import
+// ignore_for_file: unused_import, duplicate_import, unnecessary_import, use_build_context_synchronously
 
 import 'dart:ui';
 
